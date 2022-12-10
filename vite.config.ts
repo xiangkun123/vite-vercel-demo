c6db069f3924159326fbbff6b7e676982442075e
jiang.xiangkun <jiang.xiangkun@trs.com.cn>
2022-12-11 01:41:20 +0800

    初始化vite项目并配置root参数

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+
+// 引入 path 包注意两点:
+// 1. 为避免类型报错，你需要通过 `pnpm i @types/node -D` 安装类型
+// 2. tsconfig.node.json 中设置 `allowSyntheticDefaultImports: true`，以允许下面的 default 导入方式
+import path from 'path'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // 手动指定项目根目录,当需要把index.html移入src时使用
+  // root: path.join(__dirname, 'src'),
+  plugins: [react()],
+})
