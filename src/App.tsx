a4c15804127ab41ab7dacec4647791517f1dba22
jiang.xiangkun <jiang.xiangkun@trs.com.cn>
2022-12-11 16:21:42 +0800

    tailwindCSS原子化框架

--- src/App.tsx
+++ src/App.tsx
@@ -12,21 +12,12 @@ function App() {
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react w-20" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="App-header m-3">
+      <h1 className='bg-red-500'>Hello! Vite + React</h1>
+      </header>
     </div>
   )
 }
